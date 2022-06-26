3c0050a8cf8a4a523b5140ab3c9a62613af1c1cc
okokip <64718443+okokip@users.noreply.github.com>
2022-06-26 16:10:05 +0800

    Add files via upload

---
+++ rename_file.py
@@ -0,0 +1,14 @@
+import os
+
+if __name__ == '__main__':
+    file_path = input("Absolute path of the excel that would like to combine: ")
+    files = os.listdir(file_path)
+    count = 0
+    for f in files:
+        if f.endswith('.xlsx'):
+            print(f"Going to rename the file: {f}")
+            abs_file_path = file_path + '\\' + f
+            new_abs_file_name = file_path + '\\' + str(count) + "-renamed.xlsx"
+            os.rename(abs_file_path,new_abs_file_name)
+            count += 1
+    print('All excel files has been Renamed!')
